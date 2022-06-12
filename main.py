a6f3b633dfb68c45021f84bc32f5be23d475f054
Dionis <den70007.ua@gmail.com>
2022-06-12 06:13:34 +0300

    use new library

--- main.py
+++ main.py
@@ -1,32 +1,26 @@
-import asyncio
 import os
-import websockets
-import aiohttp
+import logging
+from websocket_server import WebsocketServer
 from constans import *
+import requests
 
 
-async def get_weather_data(town):
-    async with aiohttp.ClientSession() as session:
-        async with session.get('https://api.weatherapi.com/v1/current.json?key={}&q={}&aqi=no'.format(os.environ.get('API'), town)) as resp:
-            data = await resp.json()
-            icon_id = images.index(int(data['current']['condition']['icon'].split('/')[-1].split('.')[0]))
-            return 'update|{}|{}|{}|{}'.format(int(data['current']['temp_c']), data['current']['humidity'],
-                                               data['current']['is_day'], icon_id)
+def get_weather_data(town):
+    r = requests.get('https://api.weatherapi.com/v1/current.json?key={}&q={}&aqi=no'.format(os.environ.get('API'), town))
+    data = r.json()
+    icon_id = images.index(int(data['current']['condition']['icon'].split('/')[-1].split('.')[0]))
+    return 'update|{}|{}|{}|{}'.format(int(data['current']['temp_c']), data['current']['humidity'],
+                                           data['current']['is_day'], icon_id)
 
 
-async def hello(websocket, _):
-    data = await websocket.recv()
-    split_data = data.split('|')
-    print(split_data)
-    if split_data[0] == 'innit':
-        await websocket.send(await get_weather_data(split_data[1]))
+def message_received(client, server, message):
+    data = message.split('|')
+    print(data)
+    if data[0] == 'innit':
+        server.send_message(client, get_weather_data(data[1]))
 
-start_server = websockets.serve(hello, "0.0.0.0", 8765)
 
-loop = asyncio.new_event_loop()
-asyncio.set_event_loop(loop)
-loop.run_until_complete(start_server)
-try:
-    loop.run_forever()
-except KeyboardInterrupt:
-    pass
+server = WebsocketServer(host='0.0.0.0', port=8765, loglevel=logging.CRITICAL)
+server.set_fn_message_received(message_received)
+
+server.run_forever()
