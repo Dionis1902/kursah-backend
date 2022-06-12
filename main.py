d40881a9baf7ba31bb9faa76c0f5e21676f71a52
Dionis <den70007.ua@gmail.com>
2022-06-12 19:35:24 +0300

    update every 60 seconds

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import os
 import logging
+import time
+
 from websocket_server import WebsocketServer
 from constans import *
 import requests
+import threading
 
 
 def get_weather_data(town):
@@ -14,14 +17,28 @@ def get_weather_data(town):
                                            data['current']['is_day'], icon_id)
 
 
+def update_data(server: WebsocketServer, client, town):
+    while True:
+        try:
+            server.send_message(client, get_weather_data(town))
+            time.sleep(60)
+        except (Exception, ):
+            break
+
+
 def message_received(client, server, message):
     data = message.split('|')
     print(data)
     if data[0] == 'innit':
         server.send_message(client, get_weather_data(data[1]))
+        threading.Thread(target=update_data, args=(server, client, data[1]), daemon=True).start()
 
 
-server = WebsocketServer(host='0.0.0.0', port=8765, loglevel=logging.CRITICAL)
-server.set_fn_message_received(message_received)
+def main():
+    server = WebsocketServer(host='0.0.0.0', port=8765, loglevel=logging.CRITICAL)
+    server.set_fn_message_received(message_received)
+    server.run_forever()
 
-server.run_forever()
+
+if __name__ == '__main__':
+    main()
